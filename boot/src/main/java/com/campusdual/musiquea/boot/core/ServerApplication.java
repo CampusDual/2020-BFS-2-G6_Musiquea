103f1b65e470e3894a6a68fddac5ffb5d820759b
Belindavf <belibvf-17@hotmail.com>
2020-06-04 10:45:19 +0200

    Init Master branch

---
+++ boot/src/main/java/com/campusdual/musiquea/boot/core/ServerApplication.java
@@ -0,0 +1,17 @@
+package com.campusdual.musiquea.boot.core;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.campusdual.musiquea.model.core","com.campusdual.musiquea.ws.core"})
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
